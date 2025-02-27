da44cf6232f99c7cee733d8e85766ffcae7f30a5
Aunn <choprasahil.sc@gmail.com>
2025-02-26 22:27:49 -0800

    tweaks
    slightly refactoring code to include proper data

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,28 @@ import Navbar from "../../components/Navbar";
 import Typewriter from "../../components/Typewriter";
 import "./page.css";
 import Carousel from "../../components/Carousel";
+import {FiCircle, FiCode, FiFileText, FiLayers, FiLayout} from "react-icons/fi";
 
-// const projects = [
-//     { title: "Snake AI", description: "A genetic algorithm for Snake", github: "https://github.com/yourrepo/snake-ai", image: "https://picsum.photos/200/300" },
-//     { title: "Casino Bot", description: "A Discord bot for online casino games", github: "https://github.com/yourrepo/casino-bot", image: "https://picsum.photos/200/300" },
-//     { title: "The Button", description: "A web app based on 'The Button' game", github: "https://github.com/yourrepo/the-button", image: "https://picsum.photos/200/300" },
-// ];
+const projects = [
+    {
+        title: "Snake AI",
+        description: "A genetic algorithm for Snake",
+        id: 1,
+        icon: <FiFileText className="carousel-icon" />,
+    },
+    {
+        title: "Casino Bot",
+        description: "A Discord bot for online casino games",
+        id: 2,
+        icon: <FiCircle className="carousel-icon" />,
+    },
+    {
+        title: "The Button",
+        description: "A web app based on 'The Button' game",
+        id: 3,
+        icon: <FiLayers className="carousel-icon" />,
+    },
+];
 
 export default function Home() {
     return (
@@ -17,6 +33,7 @@ export default function Home() {
                 <h1 className="text-4xl fon t-bold">Hi, I'm Sahil. I make</h1>
                 <Typewriter texts={["websites.", "backend.", "passion projects."]} period={2000}/>
                 <Carousel
+                    items={projects}
                     baseWidth={600}
                     autoplay={true}
                     autoplayDelay={3000}
